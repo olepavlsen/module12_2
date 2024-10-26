d4c8657245d0e44266eeec0d582ae9ddc4851c65
olepavlsen <olepavlsen@mail.ru>
2024-10-26 16:57:10 +0300

    Add files via upload

--- tests_12_2.py
+++ tests_12_2.py
@@ -21,21 +21,28 @@ class TournamentTest(unittest.TestCase):
 
     def test_run_1(self):
         run_1 = runner_and_tournament.Tournament(90, self.hus, self.nick)
-        all_results = runner_and_tournament.Tournament.start(run_1)  # finishers
-        self.assertTrue(all_results[max(all_results)] == self.nick)
-        # return all_results
+        result_1 = runner_and_tournament.Tournament.start(run_1)  # finishers
+        self.assertTrue(result_1[max(result_1)] == self.nick)
+        for i in range(len(result_1)):
+            self.all_results[i + 1] = result_1[i + 1].name
+
+
 
     def test_run_2(self):
         run_2 = runner_and_tournament.Tournament(90, self.andr, self.nick)
-        all_results = runner_and_tournament.Tournament.start(run_2)  # finishers
-        self.assertTrue(all_results[max(all_results)] == self.nick)
-        # return all_results
+        result_2 = runner_and_tournament.Tournament.start(run_2)  # finishers
+        self.assertTrue(result_2[max(result_2)] == self.nick)
+        for i in range(len(result_2)):
+            self.all_results[i + 1] = result_2[i + 1].name
+
 
     def test_run_3(self):
         run_3 = runner_and_tournament.Tournament(90, self.hus, self.andr, self.nick)
-        all_results = runner_and_tournament.Tournament.start(run_3)  # finishers
-        self.assertTrue(all_results[max(all_results)] == self.nick)
-        # return all_results
+        result_3 = runner_and_tournament.Tournament.start(run_3)  # finishers
+        self.assertTrue(result_3[max(result_3)] == self.nick)
+        for i in range(len(result_3)):
+            self.all_results[i + 1] = result_3[i + 1].name
+
 
     @classmethod
     def tearDownClass(cls):
