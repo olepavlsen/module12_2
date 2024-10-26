df65115dcb273c912fb371cb8ce9a807cdc7682e
olepavlsen <olepavlsen@mail.ru>
2024-10-26 19:42:15 +0300

    Add files via upload

--- tests_12_2.py
+++ tests_12_2.py
@@ -14,44 +14,35 @@ class TournamentTest(unittest.TestCase):
         cls.all_results = {}
 
     def test_run_1(self):
+        self.res = {}
         run_1 = runner_and_tournament.Tournament(90, self.hus, self.nick)
         result_1 = runner_and_tournament.Tournament.start(run_1)  # finishers
         self.assertTrue(result_1[max(result_1)] == self.nick)
         for place, runner in result_1.items():
-            self.all_results[place] = runner.name
-            # for i in range(len(result_1)):
-        #     self.all_results[i + 1] = result_1[i + 1].name
-
-
+            self.res[place] = runner.name
+        self.all_results[1] = self.res
 
     def test_run_2(self):
+        self.res = {}
         run_2 = runner_and_tournament.Tournament(90, self.andr, self.nick)
         result_2 = runner_and_tournament.Tournament.start(run_2)  # finishers
         self.assertTrue(result_2[max(result_2)] == self.nick)
         for place, runner in result_2.items():
-            self.all_results[place] = runner.name
-            # for i in range(len(result_2)):
-        #     self.all_results[i + 1] = result_2[i + 1].name
-
-    # def tearDown(self):
-    #     print(self.all_results)
+            self.res[place] = runner.name
+        self.all_results[2] = self.res
 
     def test_run_3(self):
+        self.res = {}
         run_3 = runner_and_tournament.Tournament(90, self.hus, self.andr, self.nick)
         result_3 = runner_and_tournament.Tournament.start(run_3)  # finishers
         self.assertTrue(result_3[max(result_3)] == self.nick)
         for place, runner in result_3.items():
-            self.all_results[place] = runner.name
-            # for i in range(len(result_3)):
-        #     self.all_results[i + 1] = result_3[i + 1].name
+            self.res[place] = runner.name
+        self.all_results[3] = self.res
 
-    def tearDown(self):
-        print(self.all_results)
-    # @classmethod
-    # def tearDownClass(cls):
-    #     print(cls.all_results)
-        # print(cls.test_run_2)
-        # print(cls.test_run_3)
+    @classmethod
+    def tearDownClass(cls):
+        print(f'{cls.all_results[1]} \n{cls.all_results[2]} \n{cls.all_results[3]}')
 
 
 if __name__ == "__main__":
